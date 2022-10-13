3ce3c4e833c29ed58f3aeeaab19316155dcfaf8a
peechidi <preciouschidi20@gmail.com>
2022-10-13 15:12:43 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,7 +4,7 @@
  */
 
 #include <stdio.h>
-
+#include <unistd.h>
 /**
  * main - and that piece of art is useful" - Dora Korpar, 2015-10-19,
  * followed by a new line.
@@ -12,6 +12,6 @@
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 58);
 	return (1);
 }
