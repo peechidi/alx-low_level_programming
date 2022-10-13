d5bee98e7891ba0f754ce3740cd4a8b04364ec61
peechidi <preciouschidi20@gmail.com>
2022-10-13 13:20:04 -0700

    Betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,18 @@
+/*
+ * File: 4-puts.c
+ * Auth: Chidi-Obinichi Precious <alxschool.io>
+ */
+
 #include <stdio.h>
-int main (void) 
+
+/**
+ * main - Prints "Programming is like building a multilingual
+ * puzzle, followed by a line.
+ *
+ * Return: Always 0.
+ */
+int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-} 
+}
