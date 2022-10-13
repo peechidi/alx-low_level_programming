9ee92b0796d0624b9390f0ddc49d24198e091726
peechidi <preciouschidi20@gmail.com>
2022-10-13 14:26:21 -0700

    main

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,7 +6,7 @@
 #include <stdio.h>
 
 /**
- * main - Prints size of various types on the computer it is compiled and run on.
+ * main - Prints size of various types on the computer it is compiled.
  *
  * Return: 0.
  */
