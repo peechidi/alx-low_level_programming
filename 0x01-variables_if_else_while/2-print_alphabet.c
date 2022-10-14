3313507857a370fe28508279b91c4b7f04381994
peechidi <preciouschidi20@gmail.com>
2022-10-14 16:03:50 -0700

    commit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio>
+/**
+ * main - This code prints out the alphabet.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+int i;
+for (i = 0; i < 26; i++)
+{
+	putchar(alp[i]);
+}
+putchar('\n');
+return (0);
+}
---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio>
+/**
+ * main - This code prints out the alphabet.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+int i;
+for (i = 0; i < 52; i++)
+{
+	putchar(alp[i]);
+}
+putchar('\n');
+return (0);
+}
