9fc301a909e4002e239a99c9021e32b4e249f7ab
peechidi <preciouschidi20@gmail.com>
2022-11-16 16:44:30 -0800

    task_1_check

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -9,13 +9,13 @@
 
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-if (array == NULL || action == NULL)
-return;
+	if (array == NULL || action == NULL)
+		return;
 
-while (size-- > 0)
-{
-action(*array);
-array++;
-}
+	while (size-- > 0)
+	{
+		action(*array);
+		array++;
+	}
 
 }
